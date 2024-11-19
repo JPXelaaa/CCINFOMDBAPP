2927d411131378604cfc99d11453d5db8e36e43b
ChrystelMarcelo <chrystel_anne_marcelo@dlsu.edu.ph>
2024-11-19 23:00:16 +0800

    Update book_record_management_menu.java

--- CCINFO209DBAPP/book_record_management_menu.java
+++ CCINFO209DBAPP/book_record_management_menu.java
@@ -32,79 +32,98 @@ public class book_record_management_menu {
                     book_record_management bm = new book_record_management();
 
                     System.out.println("Enter Book Information:");
-                    System.out.println("ISBN:");
-                    System.out.println("Title:");
-                    System.out.println("Author:");
-                    System.out.println("Genre:");
-                    System.out.println("Publisher:");
-                    System.out.println("Publication Year:");
-                    System.out.println("Stock Quantity:");
+                    System.out.println("ISBN:");                        bm.ISBN                 = Integer.parseInt(sc.nextLine());
+                    System.out.println("Title:");                       bm.title                = sc.nextLine();
 
-                    bm.add_product();
+                    System.out.println("Authors (comma-separated):");
+                    String[] authors = sc.nextLine().split(",");
+
+                    System.out.println("Genres: (comma-separated)");
+                    String[] genres = sc.nextLine().split(",");
+
+                    System.out.println("Publisher:");                   bm.publisherName          = sc.nextLine();
+                    System.out.println("Publication Year (YYYY):");     bm.publicationYear      = Integer.parseInt(sc.nextLine());
+                    System.out.println("Stock Quantity:");              bm.stockQuantity        = Integer.parseInt(sc.nextLine());
+
+
+                    if(bm.check_authors(authors) && bm.check_genres(genres)){
+                        bm.add_book();
+                    } else {
+                        System.out.println("Author or Genre does not exist. Book addition aborted.");
+                    }
+                    break;
 
                 case 2:
-                    book_record_management bm = new book_record_management();
+                    bm = new book_record_management();
                     System.out.println("Enter Book Information:");
-                    System.out.println("ISBN     :");
-                    bm.ISBN = sc.nextLine();
+                    System.out.println("ISBN     :");                   bm.ISBN = Integer.parseInt(sc.nextLine());
 
-                    if (bm.get_book() == 0) {
+                    if (!bm.get_book()) {
                         System.out.println("That product does not exist on the records");
                     } else {
                         System.out.println("Current Book Information");
                         System.out.println("-------------------------------------------------------");
                         System.out.println("ISBN             :" + bm.ISBN);
                         System.out.println("Title               :" + bm.title);
-                        System.out.println("Author              :" + bm.author);
-                        System.out.println("Genre               :" + bm.genre);
-                        System.out.println("Publisher           :" + bm.publisherID);
+                        System.out.println("Author              :" + bm.get_authors());
+                        System.out.println("Genre               :" + bm.get_genres());
+                        System.out.println("Publisher           :" + bm.publisherName);
                         System.out.println("Publication Year    :" + bm.publicationYear);
                         System.out.println("Stock Quantity      :" + bm.stockQuantity);
 
                         System.out.println("Enter Updated Book Information");
                         System.out.println("-------------------------------------------------------");
-                        System.out.println("ISBN                : ");
-                        bm.ISBN = sc.nextLine();
-                        System.out.println("Title               : ");
-                        bm.title = sc.nextLine();
-                        System.out.println("Author              : ");
-                        bm.author = sc.nextLine();
-                        System.out.println("Genre               : ");
-                        bm.genre = sc.nextLine();
-                        System.out.println("Publisher           : ");
-                        bm.publisherID = sc.nextLine();
-                        System.out.println("Publication Year    : ");
-                        bm.publicationYear = sc.next();
-                        System.out.println("Stock Quantity      : ");
-                        bm.stockQuantity = Integer.parseInt(sc.nextLine());
+                        System.out.println("ISBN                : ");       bm.ISBN = Integer.parseInt(sc.nextLine());
+                        System.out.println("Title               : ");       bm.title = sc.nextLine();
 
-                        // bm.update_book();
+
+                        System.out.println("Authors (comma-separated, must already exist): ");
+                        String[] updatedAuthors = sc.nextLine().split(",");
+                        if (!bm.check_authors(updatedAuthors)) {
+                            System.out.println("One or more authors do not exist. Update aborted.");
+                            break;
+                        }
+
+
+                        System.out.println("Genres (comma-separated, must already exist): ");
+                        String[] updatedGenres = sc.nextLine().split(",");
+                        if (!bm.check_genres(updatedGenres)) {
+                            System.out.println("One or more genres do not exist. Update aborted.");
+                            break;
+                        }
+
+
+                        System.out.println("Publisher           : ");       bm.publisherName = sc.nextLine();
+                        System.out.println("Publication Year    : ");       bm.publicationYear = Integer.parseInt(sc.nextLine());
+                        System.out.println("Stock Quantity      : ");       bm.stockQuantity = Integer.parseInt(sc.nextLine());
+
+                        bm.update_book(updatedAuthors, updatedGenres);
                     }
-
+                    break;
                 case 3:
-                    book_record_management bm = new book_record_management();
+                    bm = new book_record_management();
 
                     System.out.println("Enter Book Information");
                     System.out.println("ISBN     :");
-                    bm.ISBN = sc.nextLine();
+                    bm.ISBN = Integer.parseInt(sc.nextLine());
 
-                    // bm.delete_book();
+                    bm.delete_book();
 
                 case 4:
-                    book_record_management bm = new book_record_management();
+                    bm = new book_record_management();
 
                     System.out.println("Enter Book Information");
                     System.out.println("Book ID     :");
-                    bm.ISBN = sc.nextLine();
+                    bm.ISBN = Integer.parseInt(sc.nextLine());
 
                     bm.get_book();
                     System.out.println("Current Book Information");
                     System.out.println("-------------------------------------------------------");
                     System.out.println("ISBN                :" + bm.ISBN);
                     System.out.println("Title               :" + bm.title);
-                    System.out.println("Author              :" + bm.author);
-                    System.out.println("Genre               :" + bm.genre);
-                    System.out.println("Publisher           :" + bm.publisherID);
+                    System.out.println("Author              :" + bm.get_authors());
+                    System.out.println("Genre               :" + bm.get_genres());
+                    System.out.println("Publisher           :" + bm.publisherName);
                     System.out.println("Publication Year    :" + bm.publicationYear);
                     System.out.println("Stock Quantity      :" + bm.stockQuantity);
 
