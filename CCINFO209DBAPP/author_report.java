bfed4a26001a03332602eb00b42f8033555628b4
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-11-23 05:07:46 +0800

    Update author_report.java

--- CCINFO209DBAPP/author_report.java
+++ CCINFO209DBAPP/author_report.java
@@ -2,8 +2,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class author_report {
+	Scanner scanner = new Scanner(System.in);
 	
 	public int year;
 	public int month;
@@ -14,11 +16,18 @@ public class author_report {
 
 	public int generate_authorreport () {
 		recordcount = 0;
+		
+		System.out.print("Enter the year: ");
+        year = scanner.nextInt();
+
+        System.out.print("Enter the month (1-12): ");
+        month = scanner.nextInt();
+		
 		try {
 			Connection conn = DriverManager.getConnection("jdbc:mysql://34.57.40.219:3306/CCINFO209DB?useTimezone=true&serverTimezone=UTC&user=root&password=DLSU1234!");
 			System.out.println("Connection to DB Successful");
 			PreparedStatement pstmt = conn.prepareStatement("SELECT 	 a.pen_name, \r\n"
-														  + "			 CONCAT(a.first_name, a.middle_initial, a.last_name)	AS  AuthorName,\r\n"
+														  + "			 CONCAT(a.first_name, ' ', a.middle_initial, '. ', a.last_name)	AS  AuthorName,\r\n"
 														  + "            SUM(od.quantity_ordered) 								AS numberOfBooksSold\r\n" 
 														  + "FROM		 authors a			JOIN book_authors ba	ON a.pen_name = ba.pen_name\r\n"
 														  + "								JOIN books b			ON ba.book_ID = b.book_ID\r\n"
@@ -37,9 +46,11 @@ public class author_report {
 			
 			while (rs.next()) {
 				recordcount++;
-				System.out.printf("%-45s %-91s %d\n",rs.getString("a.pen_name"), rs.getString("AuthorName"), rs.getInt("numberOfBooksSold"));
+				System.out.printf("\nPen Name: %-15s "
+						+ "\nFull Name: %-10s "
+						+ "\nNumber of Books Sold: %-5d\n",rs.getString("a.pen_name"), rs.getString("AuthorName"), rs.getInt("numberOfBooksSold"));
 			}
-			System.out.println("End of Report");
+			System.out.println("\nEnd of Report");
 
 			pstmt.close();
 			conn.close();
